de7dac4c7ee51b7e1fd0642df8e9c70f0ade1f70
Gabor Jenei <jeneigabor14@gmail.com>
2021-07-27 07:40:15 +0100

    Add conditional control statement to template

--- app/routes.py
+++ app/routes.py
@@ -6,15 +6,4 @@ from flask import render_template
 @app.route('/index')
 def index():
     user = {'username': 'Gabor'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Bristol!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was cool!'
-        }
-    ]
-
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', user=user)
